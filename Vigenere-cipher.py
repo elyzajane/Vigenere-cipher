ba92ab707cd7f585412448deb01887798e3a777f
Elyza <elyzajanerelucio@gmail.com>
2023-04-21 23:26:54 +0800

    Key character, character value, cipher value

--- Vigenere-cipher.py
+++ Vigenere-cipher.py
@@ -4,6 +4,15 @@ def vigenere_cipher(plaintext, keyword):
     plaintext = plaintext.replace("","").upper()
     keyword = keyword.upper()
     ciphertext = ""
+    for i in range(len(plaintext)):
+        character = plaintext
+        key_character = keyword[i % len(keyword)]
+        character_value = ord(character) - ord('A')
+        key_character_value = ord(key_character) - ord('A')
+        cipher_value = (character_value + key_character_value) % 26
+        cipher_character = chr(cipher_value + ord('A'))
+        ciphertext += cipher_character
+    return ciphertext
 #ask user to input for plaintext and keyword
 #encrypt the plaintext using vigenere cipher
 #print the output
